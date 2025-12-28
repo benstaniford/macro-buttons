9e46e5dc306280e6b78fcb2a4f4720e1f4046526
Ben Staniford <bstaniford@beyondtrust.com>
2025-12-28 11:27:30 +0000

    remove unused System.Windows namespace import

--- MacroButtons/Services/BuiltinService.cs
+++ MacroButtons/Services/BuiltinService.cs
@@ -1,5 +1,3 @@
-using System.Windows;
-
 namespace MacroButtons.Services;
 
 /// <summary>
@@ -18,7 +16,7 @@ public class BuiltinService
         switch (normalized)
         {
             case "quit":
-                Application.Current.Shutdown();
+                System.Windows.Application.Current.Shutdown();
                 break;
 
             default:
