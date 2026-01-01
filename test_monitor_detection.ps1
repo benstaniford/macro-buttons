b280a3a0b3c7a62b54e02bb9a88baf061ba8e2fe
Ben Staniford <bstaniford@beyondtrust.com>
2026-01-01 14:32:33 +0000

    remove test_monitor_detection.ps1 script

--- test_monitor_detection.ps1
+++
@@ -1,9 +0,0 @@
-# PowerShell script to test monitor detection
-Add-Type -AssemblyName System.Windows.Forms
-$screens = [System.Windows.Forms.Screen]::AllScreens
-for ($i = 0; $i -lt $screens.Count; $i++) {
-    $screen = $screens[$i]
-    $bounds = $screen.Bounds
-    $area = $bounds.Width * $bounds.Height
-    Write-Host "Monitor $i`: $($bounds.Width)x$($bounds.Height) @ ($($bounds.Left),$($bounds.Top)) - Area: $area - Primary: $($screen.Primary)"
-}
